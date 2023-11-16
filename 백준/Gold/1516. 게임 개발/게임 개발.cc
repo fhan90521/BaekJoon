ae1ba79905f0ebdcb9063710b29f43dffbe71363
fhan90521 <77820191+fhan90521@users.noreply.github.com>
2023-11-16 19:17:45 +0900

    [Gold III] Title: 게임 개발, Time: 4 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Gold/1516. 게임 개발/게임 개발.cc
@@ -0,0 +1,79 @@
+// 1516.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+#define MIN(a, b) (((a) < (b)) ? (a) : (b))
+#define MAX(a, b) (((a) > (b)) ? (a) : (b))
+using namespace std;
+vector<vector<long>> g_graph;
+vector<long> selfcosts;
+vector<long> indegree;
+vector<int> costs;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    long N;
+    cin >> N;
+    g_graph = vector<vector<long>>(N + 1);
+    selfcosts = vector<long>(N + 1);
+    indegree = vector<long>(N + 1);
+    costs = vector<int>(N + 1);
+    for (int i = 1; i <= N; i++)
+    {
+        int cost;
+        cin >> cost;
+        selfcosts[i] = cost;
+        while (1)
+        {
+            int n;
+            cin >> n;
+            if (n == -1)
+            {
+                break;
+            }
+            g_graph[n].push_back(i);
+            indegree[i]++;
+        }
+    }
+    list<long> queue;
+    for (int i = 1; i <= N; i++)
+    {
+        if (indegree[i] == 0)
+        {
+            queue.push_back(i);
+        }
+    }
+    while (!queue.empty())
+    {
+        long curN = queue.front();
+        queue.pop_front();
+        for (long neighbor : g_graph[curN])
+        {
+            costs[neighbor] = MAX(costs[neighbor], costs[curN]+selfcosts[curN]);
+            indegree[neighbor]--;
+            if (indegree[neighbor] == 0)
+            {
+                queue.push_back(neighbor);
+            }
+        }
+    }
+    for (long i = 1; i <= N; i++)
+    {
+        cout << costs[i]+selfcosts[i] << '\n';
+    }
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
